01752d8a2847cf1b37a75e347ec56cd81b200ac8
ArturT <mr.arturio@gmail.com>
2024-05-29 22:06:06 -0400

    best practice to update state value based on old state

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
-  const [editing, setEditing] = useState(false);
+  const [isEditing, setEditing] = useState(false);
 
   function handleEdit() {
-    setEditing(true);
+    setEditing((prev) => !prev);
   }
 
   return (
     <li>
       <span className="player">
-        {editing ? (
-          <input type="text" required />
+        {isEditing ? (
+          <input type="text" required value={name}/>
         ) : (
           <span className="player-name">{name}</span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEdit}>Edit</button>
+      <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
