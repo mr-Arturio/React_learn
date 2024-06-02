557a0689216b81da50442388e32e903645d81810
ArturT <mr.arturio@gmail.com>
2024-06-01 23:17:28 -0400

    lifting state up concept

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setEditing] = useState(false);
 
@@ -15,7 +15,7 @@ function handleChange(event){
 }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {isEditing ? (
           <input type="text" required value={playerName} onChange={handleChange}/>
