a67ff15dac988c072023f77825640e8d49dbed5e
ArturT <mr.arturio@gmail.com>
2024-06-03 13:57:45 -0400

    avoid intersecting + better state management

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 
 const initialGameBoard = [
   [null, null, null],
@@ -6,18 +5,29 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+//derive the game board state from the initial game board and the turns array
+let gameBoard = initialGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+  for (const turn of turns) {
+    const { square, player } = turn; //destructuring the turn object to get the square and player properties(from App component)
+    const { row, col } = square; //destructuring the square object to get the row and col properties
+    //now we got player symbol and the row and col of the square where the player placed their symbol
 
-    onSelectSquare();
+    gameBoard[row][col] = player; //update the gameBoard array with the player symbol at the specified row and col
   }
+
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+
+  //   onSelectSquare();
+  // }
 
   return (
     <ol id="game-board">
@@ -26,7 +36,8 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                {/* to receive and pass data on which row and column clicked */}
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
---
+++ tic-tac-toe/src/components/Log.jsx
@@ -0,0 +1,10 @@
+export default function Log() {
+  return (
+    <ol id="log">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ol>
+  );
+}
