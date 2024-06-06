10d3b350f28350c3daa66d77cddc3deb1381088f
ArturT <mr.arturio@gmail.com>
2024-06-05 22:54:17 -0400

    reducing state management

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,18 +4,24 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]); //using to update two different parts of the UI
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
       //every turn will be an object of this shape:
       //square propperty which holds some nested object as a value (col, row) and a player property which holds the symbol of the active player as a value.
       const updatedTurns = [
@@ -44,10 +50,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard 
-         onSelectSquare={handleSelectSquare} 
-         turns={gameTurns} 
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
       <Log turns={gameTurns} />
     </main>
