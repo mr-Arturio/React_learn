5d20a5e5135e63464366eb247bab4ecd08cc40d7
ArturT <mr.arturio@gmail.com>
2024-06-07 22:51:56 -0400

    add Header and UserInput components

---
+++ invest_calculator/src/components/UserInput.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useState } from "react";
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+  });
+
+  function handleChange(input, value) {
+    setUserInput((prev) => {
+      return {
+        ...prev,
+        [input]: value,
+      };
+    });
+  }
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment:</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label>Annual Investment:</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
