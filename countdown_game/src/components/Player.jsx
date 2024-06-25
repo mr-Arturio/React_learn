de24f00c0b65719b35cedc341ea2eca4f6d5cc8d
ArturT <mr.arturio@gmail.com>
2024-06-25 16:06:38 -0400

    managing user input

--- countdown_game/src/components/Player.jsx
+++ countdown_game/src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name, setName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleSetName(event) {
+    setSubmitted(false);
+    setName(event.target.value);
+  }
+
+  function handleClick(){
+    setSubmitted(true);
+
+  }
+  
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? name : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleSetName} value={name}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
